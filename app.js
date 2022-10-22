0c3c651b2d381b9444e021b8dbb988b8f8f65be3
Darth-Ness <reeuuthdc80j@opayq.net>
2022-10-22 09:34:19 -0400

    Enhancement: Open pages in new tab

--- app.js
+++ app.js
@@ -6,46 +6,46 @@ var search = document.getElementById("search");
 			}
 			else {
 			if (search.value.indexOf('!b') != -1) {
-   	        	window.location.href = "https://www.scribd.com/search?query=" + search.value.slice(2, search.value.length);
+   	        	window.open("https://www.scribd.com/search?query=" + search.value.slice(2, search.value.length), "_blank");
 			}	
 			if (search.value.indexOf('!y') != -1) {
-   	        	window.location.href = "https://www.youtube.com/results?search_query=" + search.value.slice(2, search.value.length);
+   	        	window.open("https://www.youtube.com/results?search_query=" + search.value.slice(2, search.value.length), "_blank");
 			}	
 			if (search.value.indexOf('!s') != -1) {
-   	       		window.location.href = "https://www.startpage.com/sp/search?query=" + search.value.slice(2, search.value.length);
+   	       		window.open("https://www.startpage.com/sp/search?query=" + search.value.slice(2, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!e') != -1) {
-   	       		window.location.href = "https://www.ecosia.org/search?q=" + search.value.slice(2, search.value.length);
+   	       		window.open("https://www.ecosia.org/search?q=" + search.value.slice(2, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!m') != -1) {
-   	       		window.location.href = "https://www.imdb.com/find?q=" + search.value.slice(2, search.value.length);
+   	       		window.open("https://www.imdb.com/find?q=" + search.value.slice(2, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!g') != -1) {
-   	       		window.location.href = "https://github.com/search?q=" + search.value.slice(2, search.value.length);
+   	       		window.open("https://github.com/search?q=" + search.value.slice(2, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!w') != -1 && search.value != "!weather") {
-   	       		window.location.href = "https://www.walmart.com/search?q=" + search.value.slice(2, search.value.length);
+   	       		window.open("https://www.walmart.com/search?q=" + search.value.slice(2, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!d') != -1) {
-   	       		window.location.href = "https://www.dictionary.com/browse/" + search.value.slice(2, search.value.length);
+   	       		window.open("https://www.dictionary.com/browse/" + search.value.slice(2, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!f') != -1) {
-   	       		window.location.href = "https://www.flickr.com/search/?text=" + search.value.slice(2, search.value.length);
+   	       		window.open("https://www.flickr.com/search/?text=" + search.value.slice(2, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!p') != -1) {
-   	       		window.location.href = "https://play.google.com/store/search?q=" + search.value.slice(2, search.value.length);
+   	       		window.open("https://play.google.com/store/search?q=" + search.value.slice(2, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!a') != -1) {
-   	       		window.location.href = "https://www.apple.com/us/search/" + search.value.slice(2, search.value.length) + "?src=serp";
+   	       		window.open("https://www.apple.com/us/search/" + search.value.slice(2, search.value.length) + "?src=serp", "_blank");
 			}
 			if (search.value.indexOf('!l') != -1) {
-   	       		window.location.href = "https://www.lyricsmode.com/search.php?search=" + search.value.slice(2, search.value.length);
+   	       		window.open("https://www.lyricsmode.com/search.php?search=" + search.value.slice(2, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!weather') != -1) {
-   	       		window.location.href = "https://www.accuweather.com/en/search-locations?query=" + search.value.slice(9, search.value.length);
+   	       		window.open("https://www.accuweather.com/en/search-locations?query=" + search.value.slice(9, search.value.length), "_blank");
 			}
 			if (search.value.indexOf('!') == -1) { 
-          		window.location.href = "https://www.mojeek.com/search?q=" + search.value;
+          		window.open("https://www.mojeek.com/search?q=" + search.value, "_blank");
 			}
 		}
         }	
