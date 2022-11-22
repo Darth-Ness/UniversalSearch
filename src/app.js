0df79dd6e097a6a690e1b0bc283dad31f3b824b4
Darth-Ness <reeuuthdc80j@opayq.net>
2022-11-22 07:15:59 -0500

    UI: Use Android 12 styled clock

--- src/app.js
+++ src/app.js
@@ -54,14 +54,10 @@ function currentTime() {
 	var date = new Date(); /* creating object of Date class */
 	var hour = date.getHours();
 	var min = date.getMinutes();
-	var sec = date.getSeconds();
-	var midday = "AM";
-	midday = (hour >= 12) ? "PM" : "AM"; /* assigning AM/PM */
 	hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* assigning hour in 12-hour format */
 	hour = updateTime(hour);
 	min = updateTime(min);
-	sec = updateTime(sec);
-	document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + " " + midday; /* adding time to the div */
+	document.getElementById("clock").innerHTML = hour + "<br>" + min;
 	  var t = setTimeout(currentTime, 1000); /* setting timer */
   }
   
